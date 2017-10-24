4f69a42d3706ce365b484974175680b021c3d2c9
Kim Larsson <kimla89@gmail.com>
2017-10-24 22:53:02 +0200

    Added loops for writing out alla dayNames and all the days in the calendar view

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="Calendar">
         <h1>Calendar</h1>
-        {{ days }}
+        <div class="days">
+            <div class="day" v-for="day in dayNames">{{ day }}</div>
+        </div>
+        <div class="days">
+            <div class="day" v-for="day in days">
+                {{ getDate(day) }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,19 +19,33 @@ import endOfMonth from 'date-fns/end_of_month';
 import startOfWeek from 'date-fns/start_of_week';
 import endOfWeek from 'date-fns/end_of_week';
 import eachDay from 'date-fns/each_day';
+import getDate from 'date-fns/get_date';
 
 export default {
     name: 'calendar',
+    data() {
+        return {
+            dayNames: [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday',
+            ],
+        };
+    },
     computed: {
         month() {
             return getMonth(new Date());
         },
         startDay() {
-            const firstInMonth = startOfMonth(this.month);
+            const firstInMonth = startOfMonth(new Date());
             return startOfWeek(firstInMonth, { weekStartsOn: 1 });
         },
         endDay() {
-            const lastInMonth = endOfMonth(this.month);
+            const lastInMonth = endOfMonth(new Date());
             return endOfWeek(lastInMonth, { weekStartsOn: 1 });
         },
         days() {
@@ -34,8 +55,22 @@ export default {
             );
         },
     },
+    methods: {
+        getDate,
+    },
 };
 </script>
 
 <style lang="css">
+.days {
+    display: flex;
+    flex-wrap: wrap;
+}
+.day {
+    width: calc(100% / 7);
+    text-align: center;
+    padding: 30px;
+    font-weight: bold;
+    font-size: 18px;
+}
 </style>
